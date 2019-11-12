765326f181a163b68158595923d954e1a33d3bcd
fmarcelo86 <fmarcelo86@gmail.com>
2019-11-11 23:25:31 -0500

    Mejoras 4

--- src/main/java/com/todo1/store/entity/Cliente.java
+++ src/main/java/com/todo1/store/entity/Cliente.java
@@ -10,18 +10,55 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	private String cedula;
 	private String nombre;
+	private String celular;
+	private String email;
+	private String direccion;
 
+	public Cliente() {
+	}
+	public Cliente(String cedula, String nombre, String celular, String email, String direccion) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.celular = celular;
+		this.email = email;
+		this.direccion = direccion;
+	}
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 }
--- src/main/java/com/todo1/store/entity/Usuario.java
+++ src/main/java/com/todo1/store/entity/Usuario.java
@@ -10,13 +10,32 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	private String cedula;
 	private String nombre;
+	private String celular;
+	private String email;
+	private String direccion;
 
+	public Usuario() {
+	}
+	public Usuario(String cedula, String nombre, String celular, String email, String direccion) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.celular = celular;
+		this.email = email;
+		this.direccion = direccion;
+	}
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
 	}
 	public String getNombre() {
 		return nombre;
@@ -24,4 +43,22 @@ public class Usuario {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}	
 }
